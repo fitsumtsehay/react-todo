c9a23c182f509689067efa865e46df6abb0bb758
Fitsum <tsehayfitsum@gmail.com>
2023-06-05 18:46:31 -0400

    my first todo list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
+
+const todoList = [  
+  {id: 1, title: 'Complete assignment' },
+  {id: 2, title: 'Take a break' },
+  {id: 3, title: 'go to bed'}
+];
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div>
       <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <h1>
+          Todo List
+          </h1>
       </header>
-    </div>
+
+      <ul>
+      {todoList.map(todo => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+      </ul>
+      </div>
   );
 }
 
