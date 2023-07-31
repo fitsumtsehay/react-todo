c8cabb4ec61fe2f16305268676714bd23a24fdd0
Fitsum <tsehayfitsum@gmail.com>
2023-07-31 00:21:36 -0400

    lesson-1.5

--- src/src/App.js
+++ src/src/App.js
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
+function useSemiPersistentState() {
+  const savedTodoList = localStorage.getItem('savedTodoList');
+  const [todoList, setTodoList] = useState(savedTodoList ? JSON.parse(savedTodoList) :[]);
+  useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  }, [todoList]);
+  return [todoList, setTodoList];
+}
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState();
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
   };
 
+ 
   return (
-    <div>
+    <>
       <h1 style={{ marginBottom: '10px' }}>My Hacker Stories</h1>
       <hr style={{ margin: '0' }} />
       <div style={{ margin: '10px' }}>
         <AddTodoForm onAddTodo={addTodo} />
       </div>
       <TodoList todoList={todoList} />
-    </div>
+    </>
   );
 }
 
