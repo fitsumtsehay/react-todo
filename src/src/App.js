3243fca5d5c4ca61f0ffabf1774435ef6fd6dd71
Fitsum <tsehayfitsum@gmail.com>
2023-10-24 01:47:37 -0400

    lesson-1.7

--- src/src/App.js
+++ src/src/App.js
@@ -2,16 +2,41 @@ import React, { useState, useEffect } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-function useSemiPersistentState() {
-  const savedTodoList = localStorage.getItem('savedTodoList');
-  const [todoList, setTodoList] = useState(savedTodoList ? JSON.parse(savedTodoList) :[]);
-  useEffect(() => {
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-  }, [todoList]);
-  return [todoList, setTodoList];
-}
 function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
+  // a loading state to indicate the data is still fetching
+  const [loading, setLoading] = useState(true);
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    // new Promise to simulate an API call
+    new Promise((resolve, reject) => {
+      // timeout to mimic a loading delay
+      setTimeout(() => {
+        // Resolve the promise with the data from localStorage
+        resolve({
+          data: {
+            todoList: JSON.parse(localStorage.getItem('savedTodoList')) || [],
+          },
+        });
+      }, 2000);
+    })
+      .then((result) => {
+        // Update the todoList state with the result
+        setTodoList(result.data.todoList);
+        setLoading(false);
+      })
+      .catch((error) => {
+        // errors handling
+        console.error(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    // Only update the localStorage if the data is not loading
+    if (!loading) {
+      localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+    }
+  }, [todoList, loading]);
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
@@ -20,7 +45,7 @@ function App() {
   const removeTodo = (id) => {
     const updatedTodoList = todoList.filter((todo) => todo.id !== id);
     setTodoList(updatedTodoList);
-  }
+  };
   return (
     <>
       <h2 style={{ marginBottom: '10px' }}>My Todo List</h2>
@@ -28,7 +53,12 @@ function App() {
       <div style={{ margin: '10px' }}>
         <AddTodoForm onAddTodo={addTodo} />
       </div>
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
+      {/* Display a loading message while the data is fetching */}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      )}
     </>
   );
 }
